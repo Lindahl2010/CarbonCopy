8ef3c33cd6f5e537cdffd4fe8d38d7a40a912f05
Eli Lindahl <linelij@dunwoody.edu>
2020-02-20 00:04:51 -0600

    Added bootstrap for CSS and JS

--- cc_app.js
+++ cc_app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const mysql = require('mysql');
 const path = require('path');
 const app = express();
 
-// Routes for the application.
+const {getHomePage} = require('./routes/index')
+const port = 5000;
+
+// Middleware Configuration
+app.set('port', process.env.PORT || port); // Sets Express to use this port
+app.set('views', __dirname + '/views'); // Sets Express to look in this folder to render our views
+app.use(express.static(path.join(__dirname, 'public'))); // Configures Express to use this public folder
+app.use(fileUpload()); // Configure file upload
+
+// Routes for the application
